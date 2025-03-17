84f13395c3183244786f16b9ea2edea1adf6d31d
Pichau <juliovernedelamancha@gmail.com>
2025-03-17 16:41:41 -0300

    aula 3

--- MauiApp5/App.xaml.cs
+++ MauiApp5/App.xaml.cs
@@ -1,12 +1,34 @@
-﻿namespace MauiApp5
+﻿using MauiApp5.Helpers;
+
+namespace MauiApp5
 {
     public partial class App : Application
     {
+        static SQLiteDatabaseHelper _db;
+
+        public static SQLiteDatabaseHelper Db
+        {
+            get
+            {
+                if (_db == null)
+                {
+
+                    string path = Path.Combine(
+                        Environment.GetFolderPath(
+                         Environment.SpecialFolder.LocalApplicationData),
+                        "banco_sqlite_compras.db3");
+
+                    _db = new SQLiteDatabaseHelper(path);
+                }
+                return _db;
+                }
+            }
+            
         public App()
         {
             InitializeComponent();
 
             MainPage = new NavigationPage(new Views.ListaProduto());
         }
+        }
     }
-}
--- MauiApp5/Views/NovoProduto.xaml.cs
+++ MauiApp5/Views/NovoProduto.xaml.cs
@@ -1,3 +1,5 @@
+using MauiApp5.Models;
+
 namespace MauiApp5.Views;
 
 public partial class NovoProduto : ContentPage
@@ -5,5 +7,26 @@ public partial class NovoProduto : ContentPage
 	public NovoProduto()
 	{
 		InitializeComponent();
+	}
+
+	private async void ToolbarItem_Clicked(object sender, EventArgs e)
+	{
+		try
+		{
+			Produto p = new Produto()
+			{
+				Descricao = txt_descricao.Text,
+				Quantidade = Convert.ToDouble(txt_quantidade.Text),
+				Preco = Convert.ToDouble(txt_preco.Text)
+
+			};
+
+			await App.Db.Insert(p);
+			await DisplayAlert("Sucesso!", "Registro Inserido", "Ok");
+		}
+		catch (Exception ex)
+		{
+			await DisplayAlert("Ops", ex.Message, "OK");
+		}
 	}
 }
